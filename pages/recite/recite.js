5ca5bfa7440a66ccd04cf1d84f9723754b1a4c2b
xing <yinxingdyx@163.com>
2021-06-06 09:08:53 +0800

    Add delete sentence function.

--- pages/recite/recite.js
+++ pages/recite/recite.js
@@ -5,10 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    fetchedCount: 0,
     sentences: [],
     activeIndex: 0,
     speed: 2000,
-    reciteInterval: null
+    reciteInterval: null,
+    loading: true
   },
 
   /**
@@ -16,7 +18,10 @@ Page({
    */
   onLoad: function (options) {
     this.fetchData()
-    this.setTitle('背记')
+    this.setTitle(' ')
+    setTimeout(() => {
+      this.setData({ loading: false })
+    }, 2000)
   },
 
   /**
@@ -72,11 +77,13 @@ Page({
     const milliseconds = this.data.speed
 
     const newInterval = setInterval(() => {
-      const newIdx = Math.floor(Math.random() * 7)
+      const dataLength = this.data.sentences.length
+      const newIdx = Math.floor(Math.random() * dataLength)
       this.setData({
         activeIndex: newIdx
       })
     }, milliseconds);
+
     this.setData({
       reciteInterval: newInterval
     })
@@ -88,15 +95,45 @@ Page({
       })
     }
   },
-  fetchData: function() {
+  fetchMore: function() {
     const db = wx.cloud.database()
+    const fetchLimit = 1
+    const skipCount = this.data.fetchedCount
+  
     db.collection('sentences')
       .orderBy('created_at', 'desc')
-      .limit(7)
+      .limit(fetchLimit)
+      .skip(skipCount)
+      .get({
+        success: res => {
+          const newSentences = [...this.data.sentences, ...res.data]
+          this.setData({
+            sentences: newSentences,
+            fetchedCount: this.data.fetchedCount + fetchLimit
+          })
+          this.start()
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '查询记录失败'
+          })
+          console.error('[数据库] [查询记录] 失败：', err)
+        }
+      })
+  },
+  fetchData: function() {
+    const db = wx.cloud.database()
+    const fetchLimit = 7
+
+    db.collection('sentences')
+      .orderBy('created_at', 'desc')
+      .limit(fetchLimit)
       .get({
         success: res => {
           this.setData({
-            sentences: res.data
+            sentences: res.data,
+            fetchedCount: this.data.fetchedCount + fetchLimit
           })
           this.start()
         },
@@ -120,5 +157,15 @@ Page({
       speed: 1500
     })
     this.start()
+  },
+  deleteItem: function(e) {
+    const itemIdx = e.currentTarget.dataset.val
+    const newSentences = [...this.data.sentences]
+    newSentences.splice(itemIdx, 1)
+
+    this.setData({
+      sentences: newSentences,
+    })
+    this.fetchMore()
   }
 })
