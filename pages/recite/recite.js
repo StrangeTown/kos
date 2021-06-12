bc5262a677368ca9a7a57f9193dfc10aad14ce88
xing <yinxingdyx@163.com>
2021-06-12 22:42:35 +0800

    Add recite progress.

--- pages/recite/recite.js
+++ pages/recite/recite.js
@@ -1,4 +1,10 @@
 // pages/recite/recite.js
+const loadingTips = [
+  '利用碎片时间背几句口语吧',
+  '删除记熟的条目，载入新的口语'
+]
+let timeInterval = null
+
 Page({
 
   /**
@@ -10,13 +16,19 @@ Page({
     activeIndex: 0,
     speed: 2000,
     reciteInterval: null,
-    loading: true
+    loading: true,
+    loadingTip: '',
+    progress: Array(7).fill(1),
+    startTime: null,
+    timeStr: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.initLoadingTip()
+    this.startTimeInterval()
     this.fetchData()
     this.setTitle(' ')
     setTimeout(() => {
@@ -50,6 +62,7 @@ Page({
    */
   onUnload: function () {
     clearInterval(this.data.reciteInterval)
+    clearInterval(timeInterval)
   },
 
   /**
@@ -158,6 +171,10 @@ Page({
     })
     this.start()
   },
+  updateProgress: function() {
+    const newProgerss = [...this.data.progress, 1]
+    this.setData({ progress: newProgerss })
+  },
   deleteItem: function(e) {
     const itemIdx = e.currentTarget.dataset.val
     const newSentences = [...this.data.sentences]
@@ -167,5 +184,34 @@ Page({
       sentences: newSentences,
     })
     this.fetchMore()
+    this.updateProgress()
+  },
+  initLoadingTip: function() {
+    const tipsLength = loadingTips.length
+    const tipIdx = Math.floor(Math.random() * tipsLength)
+    const activeTip = loadingTips[tipIdx]
+    this.setData({
+      loadingTip: activeTip
+    })
+  },
+  startTimeInterval: function() {
+    timeInterval = setInterval(() => {
+      this.updateTimeStr()
+    }, 1000);
+  },
+  updateTimeStr: function() {
+    let startDate = this.data.startTime
+    if (!startDate) {
+      startDate = new Date()
+      this.setData({ startTime: startDate })
+    }
+    const endDate = new Date()
+
+    const secondsDiff = (endDate.getTime() - startDate.getTime()) / 1000
+    const intSeconds = Number.parseInt(secondsDiff, 10)
+    const minutes = Math.floor(intSeconds / 60)
+    const seconds = intSeconds - (minutes * 60)
+    const secondsStr = seconds < 10 ? `0${seconds}` : seconds
+    this.setData({ timeStr: `${minutes}:${secondsStr}` })
   }
 })
