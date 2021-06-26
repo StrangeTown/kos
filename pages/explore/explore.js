3b5a37e09c0064d598c6c4030c8619e7b71cb231
xing <yinxingdyx@163.com>
2021-06-26 23:42:42 +0800

    Add head text in Explore.

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -1,18 +1,22 @@
 // pages/explore.js
+import get from 'lodash.get'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    sentences: [],
+    headText: '',
+    headTextInterval: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.fetchData()
   },
 
   /**
@@ -62,5 +66,53 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  initHeadText: function(sentenceIdx) {
+    const sentences = this.data.sentences
+    const sentence = get(sentences, `[${sentenceIdx}].label`)
+    const sentenceLength = sentences.length
+
+    let idx = 0
+    const lastIdx = sentence.length - 1
+    const interval = setInterval(() => {
+      if (idx > lastIdx) {
+        clearInterval(this.data.headTextInterval)
+        const nextSentenceIdx = sentenceIdx === (sentenceLength - 1) ? 0 : (sentenceIdx + 1)
+        setTimeout(() => {
+          this.initHeadText(nextSentenceIdx)
+        }, 2000);
+      } else {
+        this.setData({
+          headText: sentence.slice(0, idx + 1)
+        })
+        idx++
+      }
+    }, 360);
+    this.setData({
+      headTextInterval: interval
+    })
+  },
+  fetchData: function() {
+    const db = wx.cloud.database()
+    const fetchLimit = 10
+
+    db.collection('sentences')
+      .orderBy('created_at', 'desc')
+      .limit(fetchLimit)
+      .get({
+        success: res => {
+          this.setData({
+            sentences: res.data,
+          })
+          this.initHeadText(0)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '查询记录失败'
+          })
+          console.error('[数据库] [查询记录] 失败：', err)
+        }
+      })
   },
 })
