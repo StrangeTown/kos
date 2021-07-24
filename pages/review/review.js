a0ce7b14184a39a055d93d728a49ae208ccb3432
xing <yinxingdyx@163.com>
2021-07-24 17:34:05 +0800

    Add onReachBottom in Review.

--- pages/review/review.js
+++ pages/review/review.js
@@ -1,10 +1,9 @@
-const get = require("lodash.get")
+const get = require('lodash.get')
 
 // pages/review/review.js
 const app = getApp()
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -12,99 +11,89 @@ Page({
     sentences: [],
     modelVisible: false,
     modelItem: null,
-    typeid: ''
+    typeid: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const typeid = options.typeid
-    const typeLabel = options.type_label
-    if (typeid) {
-      this.setData({ typeid })
-    }
+    this.setPageTitle(options.type_label)
 
-    this.fetchData({ typeid })
-
+    this.setData({ typeid: options.typeid }, () => {
+      this.fetchData()
+    })
+  },
+  setPageTitle: function(title) {
     wx.setNavigationBarTitle({
-      title: typeLabel || '列表'
+      title: title || '列表',
     })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.fetchData()
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  },
-  fetchData: function({ typeid }) {
+  onShareAppMessage: function () {},
+  fetchData: function () {
     const db = wx.cloud.database()
     const whereCondition = {}
+    const { typeid } = this.data
     if (typeid) whereCondition.type_id = typeid
 
     db.collection('sentences')
       .orderBy('created_at', 'desc')
       .where(whereCondition)
-      .limit(100)
+      .limit(20)
+      .skip(this.data.sentences.length)
       .get({
-        success: res => {
+        success: (res) => {
+          const sentences = [...this.data.sentences, ...res.data]
           this.setData({
-            sentences: res.data
+            sentences,
           })
         },
-        fail: err => {
+        fail: (err) => {
           wx.showToast({
             icon: 'none',
-            title: '查询记录失败'
+            title: '查询记录失败',
           })
           console.error('[数据库] [查询记录] 失败：', err)
-        }
+        },
       })
   },
-  like: function() {
+  like: function () {
     const sentenceId = get(this.data.modelItem, '_id')
     if (!sentenceId) return
 
@@ -121,23 +110,23 @@ Page({
         wx.showToast({
           title: msg,
           icon: 'success',
-          duration: 1000
+          duration: 1000,
         })
       },
-      fail: console.error
+      fail: console.error,
     })
   },
-  focus: function(e) {
+  focus: function (e) {
     const sentenceId = e.target.dataset.id
     if (!sentenceId) return
-    
-    const modelItem = this.data.sentences.find(s => s._id === sentenceId)
+
+    const modelItem = this.data.sentences.find((s) => s._id === sentenceId)
     this.setData({
       modelItem,
-      modelVisible: true
+      modelVisible: true,
     })
   },
-  close: function() {
+  close: function () {
     this.setData({ modelVisible: false })
-  }
+  },
 })
