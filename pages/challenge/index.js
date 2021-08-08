f87b77ae4a7d19509bfe3de151dea35255428da0
xing <yinxingdyx@163.com>
2021-08-08 21:07:35 +0800

    Init challenge.

--- pages/challenge/index.js
+++ pages/challenge/index.js
@@ -1,3 +1,5 @@
+import get from 'lodash.get'
+
 // pages/challenge/index.js
 Page({
 
@@ -5,14 +7,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    sentences: [],
+    fragments: [],
+    currentIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.fetchRandomData()
   },
 
   /**
@@ -68,5 +72,35 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  fetchRandomData: async function() {
+    const db = wx.cloud.database()
+    const res = await db.collection('sentences').count()
+    const total = res.total
+
+    var arr = [];
+    while(arr.length < 5){
+      var r = Math.floor(Math.random() * total) + 1;
+      if(arr.indexOf(r) === -1) arr.push(r);
+    }
+
+    const tasks = []
+    arr.forEach(item => {
+      const skipNum = item - 1
+      const promise = db.collection('sentences').skip(skipNum).limit(1).get()
+      tasks.push(promise)
+    })
+    const allResp = await Promise.all(tasks)
+    const list = allResp.map(r => {
+      return r.data[0]
+    })
+    const fragments = list.reduce((prev, curr) => {
+      const words = get(curr, 'value', '').split(' ')
+      return [ ...prev, ...words ]
+    }, [])
+    this.setData({
+      sentences: list,
+      fragments
+    })
+  },
 })
